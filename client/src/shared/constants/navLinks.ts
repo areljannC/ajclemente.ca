fd84bcafe604e3f500fd5600a8358b69a40aa70b
AJ Clemente <areljann.c@gmail.com>
2021-10-15 14:11:48 -0400

    Create an array of nav links used by the navbar

---
+++ client/src/shared/constants/navLinks.ts
@@ -0,0 +1,27 @@
+export const NAV_LINKS = [
+  {
+    name: 'home',
+    title: 'Home',
+    route: '/'
+  },
+  {
+    name: 'experience',
+    title: 'Experience',
+    route: '/experience'
+  },
+  {
+    name: 'projects',
+    title: 'Projects',
+    route: '/projects'
+  },
+  {
+    name: 'blog',
+    title: 'Blog',
+    route: '/blog'
+  },
+  {
+    name: 'about',
+    title: 'About',
+    route: '/about'
+  }
+];
