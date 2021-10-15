b715be4c3289766591039cfaef13203bcacf9c75
AJ Clemente <areljann.c@gmail.com>
2021-10-15 14:13:58 -0400

    Create a reusable SEO component used by pages

---
+++ client/src/shared/components/SEO/SEO.tsx
@@ -0,0 +1,40 @@
+// EXTERNAL IMPORTS
+import React, { FunctionComponent, memo } from 'react';
+import Head from 'next/head';
+
+// Types
+type PropsType = {
+  seo?: {
+    title?: string;
+    description?: string;
+    keywords?: string[];
+    robots?: string[];
+    og?: {
+      siteName?: string;
+      title?: string;
+      description?: string;
+      url?: string;
+    };
+    canonical?: string;
+  };
+};
+
+// Component
+const SEO: FunctionComponent<PropsType> = (props: PropsType) => (
+  <Head>
+    <title children={props.seo.title} />
+    <meta name='description' content={props.seo.description} />
+    <meta name='robots' content={props.seo.robots.join()} />
+    <meta name='googlebot' content={props.seo.robots.join()} />
+    <meta property='og:site_name' content={props.seo.og.siteName} />
+    <meta property='og:title' content={props.seo.og.title} />
+    <meta property='og:description' content={props.seo.og.description} />
+    <meta property='og:url' content={props.seo.og.url} />
+    <link rel='canonical' href={props.seo.og.url} />
+  </Head>
+);
+
+// Display Name
+SEO.displayName = SEO.name;
+
+export default memo(SEO);
