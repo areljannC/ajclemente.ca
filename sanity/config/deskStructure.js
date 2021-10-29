38b2e1ee8f41ed590d507de933800c28d744cdd6
AJ Clemente <areljann.c@gmail.com>
2021-10-29 00:20:28 -0400

    Merge LoC to one line

--- sanity/config/deskStructure.js
+++ sanity/config/deskStructure.js
@@ -5,9 +5,7 @@ export default () =>
     .title('Content')
     .items([
       S.listItem().title('Home Page').child(S.document().schemaType('homePage').documentId('homePage')),
-      S.listItem()
-        .title('Experience Page')
-        .child(S.document().schemaType('experiencePage').documentId('experiencePage')),
+      S.listItem().title('Experience Page').child(S.document().schemaType('experiencePage').documentId('experiencePage')),
       S.listItem().title('Projects Page').child(S.document().schemaType('projectsPage').documentId('projectsPage')),
       S.listItem().title('Blog Page').child(S.document().schemaType('blogPage').documentId('blogPage')),
       S.listItem().title('About Page').child(S.document().schemaType('aboutPage').documentId('aboutPage')),
