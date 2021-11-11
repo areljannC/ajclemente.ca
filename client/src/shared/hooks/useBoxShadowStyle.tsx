1033458111230b997cfe3d13cbc34ab76ecf0612
AJ Clemente <areljann.c@gmail.com>
2021-11-11 00:30:14 -0500

    Create a hook for `<div>` elements with box shadows

--- client/src/shared/hooks/useBoxShadowStyle.tsx
+++ client/src/shared/hooks/useBoxShadowStyle.tsx
@@ -7,5 +7,5 @@ import { COLOR_MODE, COLORS } from '@shared/constants';
 // Hook
 export const useBoxShadowStyle = (): string => {
   const { colorMode } = useColorMode();
-  return `2px 2px 0px 0px ${colorMode === COLOR_MODE.DARK ? COLORS.WHITE : COLORS.BLACK}`;
+  return `-2px 2px 0px 0px ${colorMode === COLOR_MODE.DARK ? COLORS.WHITE : COLORS.BLACK}`;
 };
